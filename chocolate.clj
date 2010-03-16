bc09140c0191ec2a6ec2929d505e9453b8e77dd6
Mark Probst <schani@Test-Erichs-iMac.local>
2010-03-16 03:38:47 +0100

    win-loss-bits calculates the number of times each bit occurs in won and lost chocolates.

--- chocolate.clj
+++ chocolate.clj
@@ -1,4 +1,5 @@
-(ns at.ac.tuwien.complang.chocolate)
+(ns at.ac.tuwien.complang.chocolate
+  (:use clojure.contrib.seq-utils))
 
 (defn normalize [choc]
   (take-while (complement zero?) choc))
@@ -36,3 +37,19 @@
 
 (defn all-chocolates [rows cols]
   (map normalize (rest (all-chocolates-raw rows cols))))
+
+(defn chocolate-bits [rows cols choc]
+  (doall (let [unnormalized-choc (concat choc (repeat (- rows (count choc)) 0))]
+	   (map (fn [i]
+		  (concat (repeat i 1) (repeat (- cols i) 0)))
+		unnormalized-choc))))
+
+(defn add-chocolate-bits [bits1 bits2]
+  (doall (map #(doall (map + %1 %2)) bits1 bits2)))
+
+(defn win-loss-bits [rows cols]
+  (let [groups (group-by winning-chocolate? (all-chocolates rows cols))
+	winners (groups true)
+	losers (groups nil)]
+    [(reduce add-chocolate-bits (map #(chocolate-bits rows cols %) winners))
+     (reduce add-chocolate-bits (map #(chocolate-bits rows cols %) losers))]))
